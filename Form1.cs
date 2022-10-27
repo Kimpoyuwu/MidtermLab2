acd7a775a4576d525eabdd7305b938c3c3ef4ca2
Kim David <115102999+Kimpoyuwu@users.noreply.github.com>
2022-10-27 15:22:14 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace David_BasicThreading
+{
+
+    public partial class Form1 : Form
+    {
+        private Thread thread1, thread2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Before starting thread-");
+
+            ThreadStart delthreadStart = new ThreadStart(MyThreadClass.Thread1);
+
+            thread1 = new Thread(delthreadStart);
+            thread1.Name = "Thread A";
+
+            thread2 = new Thread(delthreadStart);
+            thread2.Name = "Thread B";
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("-End of Thread-");
+            lblThreads.Text = "-End of Thread-";
+
+
+
+
+
+
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace David_BasicThreading
+{
+    class MyThreadClass
+    {
+        public  static void Thread1()
+        {
+            for (int LoopCount = 0; LoopCount <6; LoopCount ++)
+            {
+                Thread.Sleep(1500);
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of thread: " + thread.Name + "=" + LoopCount);
+                
+            }
+        }
+
+        
+    }
+}
